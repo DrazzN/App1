9e718e960948778b0ee50e201ce1c3474d0981d9
DARKN <darkrazznakarmi@gmail.com>
2022-09-13 23:18:33 +0545

    Added Result Output

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-int n1 = 0, n2 =0;
+int n1 = 0, n2 = 0;
 string op = "";
+int result = 0;
 
 Console.WriteLine("Enter First Number : ");
 n1 = int.Parse(Console.ReadLine());
@@ -10,24 +11,25 @@ Console.WriteLine("Operator : ");
 op = Console.ReadLine();
 
 switch (op)
-    {
-        case "+":
-            result = a+b;
-            break;
-        case "-":
-            result =a - b;
-            break;
-        case "/":
-            result=a / b;
-            break;
-        case "*":
-            result=a * b;
-            break;
-        case "%":
-            result=a % b;
-            break;
-        default:
-            Console.WriteLine("Invalid Operator!!!");
-            break;
-    }
+{
+    case "+":
+        result = n1 + n2;
+        break;
+    case "-":
+        result = n1 - n2;
+        break;
+    case "/":
+        result = n1 / n2;
+        break;
+    case "*":
+        result = n1 * n2;
+        break;
+    case "%":
+        result = n1 % n2;
+        break;
+    default:
+        Console.WriteLine("Invalid Operator!!!");
+        break;
+}
+Console.WriteLine("Result : " + result);
 
