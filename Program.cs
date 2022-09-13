7ff3f936c1662e8c888e9db553e6b2d8d6590f33
DARKN <darkrazznakarmi@gmail.com>
2022-09-13 23:26:35 +0545

    Added Function

--- Program.cs
+++ Program.cs
@@ -3,33 +3,36 @@ int n1 = 0, n2 = 0;
 string op = "";
 int result = 0;
 
+void Operation(int a, int b, string h)
+{
+    switch (h)
+    {
+        case "+":
+            result = a + b;
+            break;
+        case "-":
+            result = a - b;
+            break;
+        case "/":
+            result = a / b;
+            break;
+        case "*":
+            result = a * b;
+            break;
+        case "%":
+            result = a % b;
+            break;
+        default:
+            Console.WriteLine("Invalid Operator!!!");
+            break;
+    }
+    Console.WriteLine("Result : " + result);
+}
+
 Console.WriteLine("Enter First Number : ");
 n1 = int.Parse(Console.ReadLine());
 Console.WriteLine("Enter Second Number : ");
 n2 = int.Parse(Console.ReadLine());
 Console.WriteLine("Operator : ");
 op = Console.ReadLine();
-
-switch (op)
-{
-    case "+":
-        result = n1 + n2;
-        break;
-    case "-":
-        result = n1 - n2;
-        break;
-    case "/":
-        result = n1 / n2;
-        break;
-    case "*":
-        result = n1 * n2;
-        break;
-    case "%":
-        result = n1 % n2;
-        break;
-    default:
-        Console.WriteLine("Invalid Operator!!!");
-        break;
-}
-Console.WriteLine("Result : " + result);
-
+Operation(n1,n2,op);
